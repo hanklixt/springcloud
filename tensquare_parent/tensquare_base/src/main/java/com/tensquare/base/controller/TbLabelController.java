6c28f0b322480ee9555ebedc35c391c857f38c9f
hanklixt <1033110657@qq.com>
2019-09-21 18:05:17 +0800

    简单crud

--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/TbLabelController.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/TbLabelController.java
@@ -1,6 +1,7 @@
 package com.tensquare.base.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tensquare.base.entity.TbLabel;
 import com.tensquare.base.service.ITbLabelService;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -36,7 +39,7 @@ public class TbLabelController extends BaseController {
         return this.success(label);
     }
     @PostMapping("/{id}")
-    public Mono<Result> updateById(@PathVariable("id") String id,TbLabel  label){
+    public Mono<Result> updateById(@PathVariable("id") String id,@RequestBody  TbLabel  label){
        label.setId(id);
         tbLabelService.updateById(label);
         return success();
@@ -51,12 +54,40 @@ public class TbLabelController extends BaseController {
         return success(tbLabelService.removeById(labelId));
     }
     @PostMapping("/search")
-    public Mono<Result> search(){
-        return success(tbLabelService.list());
+    public Mono<Result> search(@RequestBody TbLabel tbLabel){
+        LambdaQueryWrapper<TbLabel> tbLabelLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        getLabelWrapper(tbLabel, tbLabelLambdaQueryWrapper);
+        return success(tbLabelService.list(tbLabelLambdaQueryWrapper));
     }
+
+    /**
+     * 构造查询条件
+     * @param tbLabel
+     * @param tbLabelLambdaQueryWrapper
+     */
+    private void getLabelWrapper(@RequestBody TbLabel tbLabel, LambdaQueryWrapper<TbLabel> tbLabelLambdaQueryWrapper) {
+        if (Objects.nonNull(tbLabel.getId())){
+            tbLabelLambdaQueryWrapper.eq(TbLabel::getId,tbLabel.getId());
+        }
+        if (Objects.nonNull(tbLabel.getLabelname())){
+            tbLabelLambdaQueryWrapper.like(TbLabel::getLabelname,tbLabel.getLabelname());
+        }
+        if (Objects.nonNull(tbLabel.getCount())){
+            tbLabelLambdaQueryWrapper.eq(TbLabel::getCount,tbLabel.getCount());
+        }
+        if (Objects.nonNull(tbLabel.getState())){
+            tbLabelLambdaQueryWrapper.eq(TbLabel::getState,tbLabel.getState());
+        }
+        if (Objects.nonNull(tbLabel.getRecommend())){
+            tbLabelLambdaQueryWrapper.eq(TbLabel::getRecommend,tbLabel.getRecommend());
+        }
+    }
+
     @PostMapping("search/{pageNo}/{pageSize}")
-    public Mono<Result>searchPage(@PathVariable("pageNo") Long pageNo,@PathVariable("/pageSize") Long pageSize){
-        return success(tbLabelService.page(new Page<TbLabel>().setCurrent(pageNo).setSize(pageSize)));
+    public Mono<Result>searchPage(@PathVariable("pageNo") Long pageNo,@PathVariable("pageSize") Long pageSize,@RequestBody TbLabel tbLabel){
+        LambdaQueryWrapper<TbLabel> tbLabelLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        getLabelWrapper(tbLabel, tbLabelLambdaQueryWrapper);
+        return success(tbLabelService.page(new Page<TbLabel>().setCurrent(pageNo).setSize(pageSize),tbLabelLambdaQueryWrapper));
     }
     @PostMapping
     public Mono<Result> save(TbLabel tbLabel){
