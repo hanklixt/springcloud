18a1d338b4f53d23cb4c19ee8ee62d47f11bf0a6
hanklixt <1033110657@qq.com>
2019-09-22 16:19:14 +0800

    添加活动模块

---
+++ tensquare_parent/gathering/src/main/java/com/tensquare/gathering/controller/TbUsergathController.java
@@ -0,0 +1,20 @@
+package com.tensquare.gathering.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户关注活动 前端控制器
+ * </p>
+ *
+ * @author lxt
+ * @since 2019-09-22
+ */
+@RestController
+@RequestMapping("/gathering/tb-usergath")
+public class TbUsergathController {
+
+}
---
+++ tensquare_parent/gathering/src/main/java/com/tensquare/gathering/service/impl/TbUsergathServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tensquare.gathering.service.impl;
+
+import com.tensquare.gathering.entity.TbUsergath;
+import com.tensquare.gathering.mapper.TbUsergathMapper;
+import com.tensquare.gathering.service.ITbUsergathService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户关注活动 服务实现类
+ * </p>
+ *
+ * @author lxt
+ * @since 2019-09-22
+ */
+@Service
+public class TbUsergathServiceImpl extends ServiceImpl<TbUsergathMapper, TbUsergath> implements ITbUsergathService {
+
+}
