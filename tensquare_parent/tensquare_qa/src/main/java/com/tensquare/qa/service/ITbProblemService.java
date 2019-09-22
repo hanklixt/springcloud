598a04623562babeb1b150f4c743d352277bffa6
hanklixt <1033110657@qq.com>
2019-09-22 14:18:35 +0800

    添加问题模块crud

--- tensquare_parent/tensquare_qa/src/main/java/com/tensquare/qa/service/ITbProblemService.java
+++ tensquare_parent/tensquare_qa/src/main/java/com/tensquare/qa/service/ITbProblemService.java
@@ -1,7 +1,10 @@
 package com.tensquare.qa.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tensquare.qa.entity.TbProblem;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-09-22
  */
 public interface ITbProblemService extends IService<TbProblem> {
+
+    Page<TbProblem> newList(String labelId, Long pageNo, Long pageSize);
+
+    Page<TbProblem>  hotList(String labelId,Long pageNo,Long pageSize);
+
+    Page<TbProblem>  waitList(String labelId,Long pageNo,Long pageSize);
 
 }
--- tensquare_parent/tensquare_qa/src/main/java/com/tensquare/qa/service/impl/TbProblemServiceImpl.java
+++ tensquare_parent/tensquare_qa/src/main/java/com/tensquare/qa/service/impl/TbProblemServiceImpl.java
@@ -1,10 +1,14 @@
 package com.tensquare.qa.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tensquare.qa.entity.TbProblem;
 import com.tensquare.qa.mapper.TbProblemMapper;
 import com.tensquare.qa.service.ITbProblemService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class TbProblemServiceImpl extends ServiceImpl<TbProblemMapper, TbProblem> implements ITbProblemService {
 
+    @Autowired
+    private TbProblemMapper tbProblemMapper;
+
+    @Override
+    public Page<TbProblem> newList(String labelId, Long pageNo, Long pageSize) {
+        Page<TbProblem> tbProblemPage = new Page<>(pageNo, pageSize);
+        return tbProblemPage.setRecords(tbProblemMapper.newList(labelId,tbProblemPage));
+    }
+
+    @Override
+    public Page<TbProblem> hotList(String labelId, Long pageNo, Long pageSize) {
+        Page<TbProblem> tbProblemPage = new Page<>(pageNo, pageSize);
+        return tbProblemPage.setRecords(tbProblemMapper.hotList(labelId,tbProblemPage));
+    }
+
+    @Override
+    public Page<TbProblem> waitList(String labelId, Long pageNo, Long pageSize) {
+        Page<TbProblem> tbProblemPage = new Page<>(pageNo, pageSize);
+        return tbProblemPage.setRecords(tbProblemMapper.hotList(labelId,tbProblemPage));
+    }
 }
