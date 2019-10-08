cf20f8fb1c29e27107fef4105243616b56a4dbea
hanklixt <1033110657@qq.com>
2019-10-08 14:57:16 +0800

    整合elasticsearch,添加搜素模块

---
+++ tensquare_parent/tensquare_search/src/main/java/com/tensquare/search/respistory/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.tensquare.search.respistory;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ArticleRepository extends ElasticsearchRepository {
+
+}
