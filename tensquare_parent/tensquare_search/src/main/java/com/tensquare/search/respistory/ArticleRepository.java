62cde9b10a0777647bc22feb81ff3cf8918f92ee
hanklixt <1033110657@qq.com>
2019-10-08 15:20:06 +0800

    无语，删除搜索模块重命名，不然报莫名的错误

--- tensquare_parent/tensquare_search/src/main/java/com/tensquare/search/respistory/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tensquare.search.respistory;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ArticleRepository extends ElasticsearchRepository {
-
-}
